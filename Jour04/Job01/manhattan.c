8486b717bcf55ae56f0028b366161268940e498d
Marin Clement <clement.marin@laplateforme.io>
2023-11-28 15:07:33 +0100

    [UPDATED] manhattan add macro abs

--- Jour04/Job01/manhattan.c
+++ Jour04/Job01/manhattan.c
@@ -1,11 +1,5 @@
 #include "manhattan.h"
-
-int abs(int x) {
-    if (x < 0) {
-        return -x;
-    }
-    return x;
-}
+#define abs(x) ((x) < 0 ? -(x) : (x))
 
 int manhattan(point p1, point p2) {
     int distance = abs(p1.x - p2.x) + abs(p1.y - p2.y);
